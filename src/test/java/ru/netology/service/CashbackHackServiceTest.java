9c10ee5449bc95e25a3bb12c8e0b4734314bab1f
PROGRAMMER <motafaka@yandex.ru>
2024-01-03 06:49:18 +0300

    testForJunit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,12 +1,43 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.Assert;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
 
     @Test
-    void remain() {
+    public void less1000() {
+        CashbackHackService service = new CashbackHackService();
+        service.remain(900);
+
+        int expected = 100;
+        int actual = service.remain(900);
+
+        Assert.assertEquals(actual,expected);
+
+    }
+
+    @Test
+    public void more1000() {
+        CashbackHackService service = new CashbackHackService();
+        service.remain(1100);
+
+        int expected = 900;
+        int actual = service.remain(1100);
+
+        Assert.assertEquals(actual,expected);
+
+    }
+
+    @Test
+    public void amount1000() {
+        CashbackHackService service = new CashbackHackService();
+        service.remain(1000);
+
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        Assert.assertEquals(actual,expected);
+
     }
 }
